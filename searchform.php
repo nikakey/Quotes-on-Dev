9dec6796d891925a81b861048aa30c06156f5d1b
Veronika Kochetkova <veronica90k@gmail.com>
2018-03-01 11:45:07 -0800

    Style 404 and search pages

--- searchform.php
+++ searchform.php
@@ -4,7 +4,6 @@
 			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 		<button class="search-submit">
-			<?php echo esc_html( 'Search' ); ?>
 			<span class="icon-search">
 				<i class="fa fa-search"></i>
 			</span>
