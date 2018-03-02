2ba06f2b335ef35a55d1937dd2f1906fe9efdd9e
Veronika Kochetkova <veronica90k@gmail.com>
2018-03-02 10:55:43 -0800

    Set ajax to dynamically add a new quote to the front page without reloading the page

--- js/api.js
+++ js/api.js
@@ -1,12 +1,39 @@
 (function($){
 
-//fetch a random quote post on the front page
+    //fetch a random quote post on the front page
 
-$('body').append('');
+    $( '#new-quote-button' ).on( 'click', function ( event ) {
+        event.preventDefault();
+        $.ajax({
+            method: 'GET',
+            url: 'wp-json/wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+            success: function ( data ) {
+                var post = data.shift();
+                
+                console.log(post);
+                
+                $( '.entry-content' ).html( post.content.rendered );
+                $( '.entry-title' ).html( post.title.rendered );
 
-//history api, 
+                if ( post._qod_quote_source !== '' && post._qod_quote_source_url !== '' ) {
+                    $( '.source' ).html( ', <a href="' + post._qod_quote_source_url + '">' + post._qod_quote_source + '</a>' );
+                }
 
-//submit a new quote with the form using ajax
+                else if ( post._qod_quote_source !== '' && post._qod_quote_source_url === '' ) {
+                    $( '.source' ).html(', ' + post._qod_quote_source );
+                }
+
+                else {
+                    $( '.source' ).html( '' );
+                }
+            }
+        
+        });
+    });
+
+    //history api, 
+
+    //submit a new quote with the form using ajax
 
 
 })(jQuery);
